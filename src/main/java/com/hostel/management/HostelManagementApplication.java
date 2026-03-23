4cf87b0bb9316659aa078a757e30a24f3c21350c
Akith Rao Thanniru <akithraothanniru@gmail.com>
2026-03-23 19:58:15 +0530

    add cors config

--- src/main/java/com/hostel/management/HostelManagementApplication.java
+++ src/main/java/com/hostel/management/HostelManagementApplication.java
@@ -2,10 +2,40 @@ package com.hostel.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.hostel.management.model.User;
+import com.hostel.management.model.User.Role;
+import com.hostel.management.repository.UserRepository;
 
 @SpringBootApplication
 public class HostelManagementApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(HostelManagementApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner createAdmin(UserRepository repo) {
+        return args -> {
+            if (repo.findByUsername("admin").isEmpty()) {
+
+                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setEmail("admin@gmail.com");
+                admin.setPassword(encoder.encode("admin123")); // 🔐 encrypted
+                admin.setRole(Role.ADMIN);
+
+                repo.save(admin);
+
+                System.out.println("✅ Admin user created!");
+            } else {
+                System.out.println("⚠️ Admin already exists");
+            }
+        };
     }
 }
---
+++ src/main/java/com/hostel/management/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.hostel.management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://hostel-management-frontend-g5b4.onrender.com")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
